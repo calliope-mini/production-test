2f40650669da94f73486db6ba472269d421c64a5
thinkberg <leo@thinkberg.com>
2017-02-10 12:15:50 +0100

    test procedure for linux

--- 1.0/testprocedure.sh
+++ 1.0/testprocedure.sh
@@ -4,13 +4,24 @@ MINI=$2
 TESTFW=`dirname $0`/firmware.hex
 JLINK=`which JLinkExe`
 
-[ "$MAINTENANCE" == "" ] && MAINTENANCE=/Volumes/MAINTENANCE
-[ "$MINI" == "" ] && MINI=/Volumes/MINI
+[ "$MAINTENANCE" == "" ] && MAINTENANCE=MAINTENANCE
+[ "$MINI" == "" ] && MINI=MINI
 
 await_mount() {
   WAITSTART=$SECONDS
   MAX=$2
-  while [ ! -f "$1/DETAILS.TXT" ]; do 
+  DEVICE=$(blkid -L "$1")
+  # if the device already exists, unmount
+  if [ "$DEVICE" != "" ]; then udisksctl unmount -b $DEVICE; fi
+  # wait until the device appears
+  until (blkid -L "$1" 2>&1 >/dev/null); do 
+    WAITELAPSED=$(($SECONDS-$WAITSTART))
+    printf "\r\033[0;31mWARTE\033[0m [$1] (${WAITELAPSED}s/${MAX}s)"
+    if [ "$WAITELAPSED" -gt "$2" ]; then echo; return 1; fi
+  done
+  DEVICE=$(blkid -L "$1")
+  # wait for the device to mount
+  until (udisksctl mount -b $DEVICE 2>&1 >/dev/null); do
     WAITELAPSED=$(($SECONDS-$WAITSTART))
     printf "\r\033[0;31mWARTE\033[0m [$1] (${WAITELAPSED}s/${MAX}s)"
     if [ "$WAITELAPSED" -gt "$2" ]; then echo; return 1; fi
@@ -35,43 +46,40 @@ while(true); do
   printf "\033[0;31mCalliope mini anstecken und einlegen!\033[0m\n"
   while [ -d "$MINI" -o -d "$MAINTENANCE" ]; do sleep 1; printf "."; done
   echo
-  echo "1. Bootloader flashen"
+  echo "1. Bootloader & Interface flashen"
   while(true); do
     ELAPSED=$((SECONDS-START))
     printf "\r\033[0;31mWARTE\033[0m(${ELAPSED}s)"
-     $JLINK -if SWD -device MKL26Z128XXX4 -speed 4000 bootloader.jlink > bootloader.log
-    if (grep -q "^O\.K\." bootloader.log); then break; fi
+    $JLINK -if SWD -device MKL26Z128XXX4 -speed 4000 bootloader.jlink > bootloader.log
+    FLASHED=$(grep "^O\.K\." bootloader.log | wc -l)
+    if [ "$FLASHED" == "2" ]; then break; fi
   done
   echo
-  printf "\033[0;31m>> Calliope mini hochnehmen! <<\033[0m\n"
-  sleep 3
-  echo "2. DAPLink Firmware (KL26z) flashen"
   await_mount $MAINTENANCE 20
-  if [ "$?" != "0" ]; then continue; fi
-  /bin/cp $DAPLINK $MAINTENANCE
-  if [ "$?" != "0" ]; then
-    echo "FEHLER: Prozedur wiederholen!"; sleep 3
-    continue
-  else
-    printf "\033[0;32mOK\033[0m\n"
-  fi
-  echo "3. TEST Firmware (NRF51) flashen"
+  DEVICE=$(blkid -L $MAINTENANCE)
+  MOUNT=$(lsblk -o MOUNTPOINT -nr $DEVICE)
+  if [ "$?" == "0" ]; then touch $MOUNT/start_if.act; fi
+  printf "2. TEST Firmware (NRF51) flashen\n"
+  printf "   \033[0;31mCalliope aufnehmen!\033[0m\n"
   await_mount $MINI 20 
+  DEVICE=$(blkid -L $MINI)
+  MOUNT=$(lsblk -o MOUNTPOINT -nr $DEVICE)
   if [ "$?" != "0" ]; then continue; fi
-  /bin/cp $TESTFW $MINI
+  /bin/cp $TESTFW $MOUNT/
   if [ "$?" != "0" ]; then
     echo "FEHLER: Prozedur wiederholen!"; sleep 3
     continue
   else
     printf "\033[0;32mOK\033[0m\n"
   fi
-  while [ -d $MINI ]; do echo -n "."; sleep 1; done
+  while [ -d $MOUNT ]; do echo -n "."; sleep 1; done
   echo
   await_mount $MINI 20
-  printf "4. \033[0;31m>> RESET DRÜCKEN! <<\033[0m\n"
-  echo "   Pixel Matrix pruefen, RGB LED, Piep abwarten."
-  printf "   \033[0;31mDANACH Calliope mini abstecken\033[0m\n"
-  while [ -d $MINI ]; do echo -n "."; sleep 1; done
+  DEVICE=$(blkid -L $MINI)
+  MOUNT=$(lsblk -o MOUNTPOINT -nr $DEVICE)
+  printf "3. Pixel Matrix pruefen, RGB LED, Piep abwarten.\n"
+  printf "   \033[0;31mDANACH Calliope mini abstecken!\033[0m\n"
+  while [ -d $MOUNT ]; do echo -n "."; sleep 1; done
   echo
   echo "DONE"
 done
