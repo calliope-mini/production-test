ce78b56be01604bc791081c65ad1a50f7f688cad
thinkberg <leo@thinkberg.com>
2017-02-14 20:20:50 +0100

    add source code for test

---
+++ src/radio-frequency-test/README.md
@@ -0,0 +1,3 @@
+# radio frequency test
+
+Code that turns on the radio for CE measurements.
---
+++ src/radio-frequency-test/source/RadioFrequencyTest.cpp
@@ -0,0 +1,139 @@
+#include "MicroBit.h"
+#include "nrf.h"
+#include "nrf51.h"
+#include "nrf51_deprecated.h"
+
+MicroBit uBit;
+
+/**
+ * Initialize the clock, so we can use the radio
+ */
+static void init(void) {
+    NRF_RNG->TASKS_START = 1;
+    NRF_CLOCK->EVENTS_HFCLKSTARTED = 0;
+    NRF_CLOCK->TASKS_HFCLKSTART = 1;
+    while (NRF_CLOCK->EVENTS_HFCLKSTARTED == 0) {
+        // Do nothing.
+    }
+}
+
+/**
+ * Disable radio.
+ */
+void off(void) {
+    NRF_RADIO->SHORTS = 0;
+    NRF_RADIO->EVENTS_DISABLED = 0;
+    NRF_RADIO->TEST = 0;
+    NRF_RADIO->TASKS_DISABLE = 1;
+    while (NRF_RADIO->EVENTS_DISABLED == 0) {
+        // Do nothing.
+    }
+    NRF_RADIO->EVENTS_DISABLED = 0;
+}
+
+/**
+ * Enable transmission on a specific frequency with full power.
+ *
+ * @param frequency the frequency (channel) to send on
+ */
+void tx(uint8_t frequency) {
+    off();
+
+    NRF_RADIO->SHORTS = RADIO_SHORTS_READY_START_Msk;
+    NRF_RADIO->TXPOWER = (RADIO_TXPOWER_TXPOWER_Neg30dBm << RADIO_TXPOWER_TXPOWER_Pos);
+    NRF_RADIO->MODE = (RADIO_MODE_MODE_Nrf_2Mbit << RADIO_MODE_MODE_Pos);
+    NRF_RADIO->FREQUENCY = frequency;
+    NRF_RADIO->TEST = (RADIO_TEST_CONST_CARRIER_Enabled << RADIO_TEST_CONST_CARRIER_Pos) |
+                      (RADIO_TEST_PLL_LOCK_Enabled << RADIO_TEST_PLL_LOCK_Pos);
+    NRF_RADIO->TASKS_TXEN = 1;
+}
+
+/**
+ * Enable reception on a specific frequency.
+ */
+void rx(uint8_t frequency) {
+    off();
+
+    NRF_RADIO->SHORTS = RADIO_SHORTS_READY_START_Msk;
+    NRF_RADIO->FREQUENCY = frequency;
+    NRF_RADIO->TASKS_RXEN = 1;
+}
+
+typedef enum {
+    Radio00TX,
+    Radio00RX,
+    Radio78TX,
+    Radio78RX
+} RadioState;
+
+RadioState state = Radio00TX;
+
+void onButtonA(MicroBitEvent event) {
+    (void) event;
+    switch (state) {
+        case Radio00TX:
+            state = Radio00RX;
+            uBit.display.scroll("RX00");
+            break;
+        case Radio00RX:
+            state = Radio78TX;
+            uBit.display.scroll("TX78");
+            rx(0);
+            break;
+        case Radio78TX:
+            state = Radio78RX;
+            uBit.display.scroll("RX78");
+            tx(78);
+            break;
+        default:
+            state = Radio00TX;
+            uBit.display.scroll("TX00");
+            break;
+    }
+}
+
+void onButtonB(MicroBitEvent event) {
+    (void) event;
+    switch (state) {
+        case Radio00TX:
+            uBit.display.scroll("START TX00");
+            tx(0);
+            break;
+        case Radio00RX:
+            uBit.display.scroll("START RX00");
+            rx(0);
+            break;
+        case Radio78TX:
+            uBit.display.scroll("START TX78");
+            tx(78);
+            break;
+        case Radio78RX:
+            uBit.display.scroll("START RX78");
+            rx(78);
+            break;
+        default:
+            /* do nothing */
+            break;
+    }
+}
+
+int main() {
+    uBit.init();
+
+    uBit.serial.baud(115200);
+    uBit.serial.send("Radio Frequency Test\r\n");
+    uBit.display.scroll("OK");
+
+    // initialize radio
+    //init();
+
+    // initialize button listeners
+    uBit.messageBus.listen(MICROBIT_ID_BUTTON_A, MICROBIT_BUTTON_EVT_CLICK, onButtonA);
+    uBit.messageBus.listen(MICROBIT_ID_BUTTON_B, MICROBIT_BUTTON_EVT_CLICK, onButtonB);
+
+
+    uBit.display.clear();
+    uBit.display.scroll("READY");
+
+    while (1) uBit.sleep(1000);
+}
