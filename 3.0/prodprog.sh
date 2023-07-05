a2a2d9b1d6388cdcc651529b1a0003d512e30fff
fabianhugo <huwg@mailbox.org>
2023-07-05 17:38:04 +0200

    updates

--- 3.0/prodprog.sh
+++ 3.0/prodprog.sh
@@ -1,15 +1,30 @@
 #! /bin/bash
 MINIPATH='/media/hugo/MINI'
 MAG='\x1b[35;49;1m'
-RED='\x1b[39;41;1m'
+RED='\x1b[39;41;1m'7
 DEF='\x1b[39;49m'
 GRE='\x1b[32;49m'
 
 while true
 do
 START=$SECONDS
+FLASHED=0
 echo 0 > /sys/class/leds/led0/brightness; # turns off green ACT LED
-JLinkExe -NoGui 1 -device NRF52820_xxAA -Commandfile erase.jlink && printf "${MAG}recovered NRF52820${DEF}\n";
+
+UNPROTECTED=0
+while true; do
+echo "try recover"
+JLinkExe -NoGui 1 -device NRF52820_xxAA -Commandfile erase.jlink > erase.log
+UNPROTECTED=$(grep "O\.K\." erase.log | wc -l)
+sleep 1
+if [ "$UNPROTECTED" == "1" ]; then printf "${MAG}recovered NRF52820${DEF}\n"; break; fi
+rm erase.log
+done
+
+echo "test"
+sleep 1
+done
+JLinkExe -NoGui 1 -device NRF52820_xxAA -Commandfile erase.jlink;
 JLinkExe -NoGui 1 -device NRF52820_xxAA -Commandfile flash.jlink && printf "${MAG}flashed DAPLINK${DEF}\n";
 printf "${MAG}wait for 6 seconds until device is ready${DEF}\n"; 
 sleep 6;
