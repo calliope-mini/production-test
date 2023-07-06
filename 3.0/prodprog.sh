a6d792dd6648d117f25529582f4eb7f1a9151dfe
Hugo <hugo.woehrl@calliope.cc>
2023-07-06 18:47:00 +0200

    improves programming, failure now leads to start from the beginning, quite stable

--- 3.0/prodprog.sh
+++ 3.0/prodprog.sh
@@ -1,14 +1,15 @@
 #! /bin/bash
 
-MINIPATH='/media/hugo/MINI'
+MINIPATH="/media/$USER/MINI"
+APPLICATION_FW="microbit-prodtest_mockup.hex"
+# Change USB Firmware in flash.jlink
 MAG='\x1b[35;49;1m'
 RED='\x1b[39;41;1m'
 DEF='\x1b[39;49m'
 GRE='\x1b[32;49m'
 
-while true; do
-START=$SECONDS
-printf "${MAG}START{DEF}\n"; 
+while true; do # First loop for always on
+while true; do # Second loop to jump to when program fails
 #echo 0 > /sys/class/leds/led0/brightness; # turns off green ACT LED
 VTref=0
 FLASHED=0
@@ -22,46 +23,42 @@ VTref=${VTstring:6:1};
 if ((VTref>1)); then printf "${GRE}VTref = $VTref V :Target voltage present ${DEF}\n"; break; else printf "${RED}VTref = $VTref :target voltage not present${DEF}\n"; sleep 1; fi;
 done;
 
+printf "${MAG}START${DEF}\n"; 
 START=$SECONDS
 
-# Recover device
-while true; do
-printf "${MAG}try recover${DEF}\n"
+# Recover device / Unlock Access Port Protection (APP)
+printf "${MAG}Try recover${DEF}\n"
 JLinkExe -NoGui 1 -device NRF52820_xxAA -Commandfile erase.jlink > recover.log
 ReadAPfull=$(grep "Read AP register 3" recover.log);
-RECOVERED=$(echo ${ReadAPfull:32:32} | grep "0x00000001" -wc); # Check if one read is "0x00000001", 
-if (( "$RECOVERED" > 0 )); then printf "${GRE}recovered NRF52820${DEF}\n"; break; else printf "${RED}recovering failed ${DEF}\n"; break; break; fi;
-done;
-
+RECOVERED=$(echo ${ReadAPfull:32:32} | grep "0x00000001" -wc); # Check if the second read is "0x00000001", 
+if (( "$RECOVERED" > 0 )); then printf "${GRE}recovered NRF52820${DEF}\n"; else printf "${RED}recovering failed ${DEF}\n"; sleep 1; break; fi; # If unlocking fails start anew
 
 # Flash USB Firmware / DAPLink
-while true; do
 JLinkExe -NoGui 1 -device NRF52820_xxAA -Commandfile flash.jlink > flash.log;
 FLASHED=$(grep "^O.K." flash.log -wc);
-if (( "$FLASHED" > 0 )); then printf "${GRE}flashed NRF52820${DEF}\n"; break; else printf "${RED}flashing failed ${DEF}\n"; break; break; fi;
-done
+if (( "$FLASHED" > 1 )); then printf "${GRE}flashed NRF52820${DEF}\n"; else printf "${RED}flashing failed ${DEF}\n"; sleep 1; break; fi; # If flashing fails start anew
 
 # Flash Application Firmware / Testprogram
-while true; do
-printf "${MAG}wait for 6 seconds until device is ready${DEF}\n"; 
+printf "${MAG}wait for 6 seconds until device is ready${DEF}\n";
 sleep 6;
 if [ -d "$MINIPATH" ]; # Check if device is connected
 then
 printf "${MAG}mini is connected, start flashing NRF52833${DEF}\n"; 
 else
 printf "${RED}mini is not found${DEF}\n";
-break # break flash application firmware loop
+break # If mini is not found, start anew
 fi;
 
-cp microbit-prodtest_mockup.hex $MINIPATH; 
+cp $APPLICATION_FW $MINIPATH; # Flash application firmware
 if [[ $? = 0 ]]; then
 printf "${GRE}DONE in $(($SECONDS - $START)) seconds. ${DEF}\n";
-printf "${MAG} DISCONNECT ${DEF}\n"
-sleep 6;
+printf "${MAG}DISCONNECT THE MINI${DEF}\n"
+sleep 5;
+# Here testing needs to be implemented
 #echo 1 > /sys/class/leds/led0/brightness; # turns on green ACT LED
-break; # leaves flash device loop
+break; # Everything successful, start anew
 else printf "${RED}Flashing of NRF52833 failed ${DEF}\n"; break; break;
-fi;
-done; 
+fi; 
 
-done
+done;
+done;
