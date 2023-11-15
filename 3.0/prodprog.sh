d826dfe2c5b427aeacb9feba1b100b8811636610
fabianhugo <huwg@mailbox.org>
2023-11-15 11:58:52 +0100

    updates mounting, adds eject

--- 3.0/prodprog.sh
+++ 3.0/prodprog.sh
@@ -8,7 +8,26 @@ MAG='\x1b[35;49;1m'
 RED='\x1b[39;41;1m'
 DEF='\x1b[39;49m'
 GRE='\x1b[32;49m'
-
+await_mount() {
+  WAITSTART=$SECONDS
+  MAX=$2
+  DEVICE=$(blkid -L "$1" 2>/dev/null)
+  # wait until the device appears
+  until (blkid -L "$1" 1>/dev/null 2>/dev/null); do 
+    WAITELAPSED=$(($SECONDS-$WAITSTART))
+    printf "\r\x1b[35;49;1mWait for device${DEF} [$1] (${WAITELAPSED}s/${MAX}s)"
+    if [ "$WAITELAPSED" -gt "$2" ]; then echo; return 1; fi
+  done
+  DEVICE=$(blkid -L "$1")
+  # wait for the device to mount
+  until (udisksctl mount -b $DEVICE 1>/dev/null 2>/dev/null); do
+    WAITELAPSED=$(($SECONDS-$WAITSTART))
+    printf "\r\x1b[35;49;1mWait for device${DEF} [$1] (${WAITELAPSED}s/${MAX}s)"
+    if [ "$WAITELAPSED" -gt "$2" ]; then echo; return 1; fi
+  done
+  printf "\n"
+  return 0
+}
 # LEDs on Header on GPIO 21 and 7, https://simonprickett.dev/controlling-raspberry-pi-gpio-pins-from-bash-scripts-traffic-lights/
 IF_DONE_LED=21
 APP_DONE_LED=7
@@ -43,7 +62,7 @@ sudo chmod 666 /sys/class/leds/ACT/brightness # make internal ACT led accessible
 
 while true; do # First loop for always on
     while true; do # Second loop to jump to when program fails
-        echo 0 > /sys/class/leds/ACT/brightness; turns off green ACT LED
+        echo 0 > /sys/class/leds/ACT/brightness; # turns off green ACT LED
         VTref=0
         FLASHED=0
         RECOVERED=0
@@ -73,36 +92,22 @@ while true; do # First loop for always on
         if (( "$FLASHED" > 1 )); then printf "${GRE}NRF52820: flashed${DEF}\n"; setLightState $IF_DONE_LED 1; else printf "${RED}NRF52820: flashing failed ${DEF}\n"; sleep 1; break; fi; # If flashing fails start anew
         
         # Flash Application Firmware / Testprogram
-        printf "${MAG}Wait 6 seconds until device is ready${DEF}\n";
-        sleep 6;
-        TRIES=8
-        i=0
-        while [ $i -le $TRIES ]
-        do
-            DEVICE=$(blkid -L "MINI" 2>/dev/null) # get the device name e.g. /dev/sda
-            MOUNT=$(lsblk -o MOUNTPOINT -nr $DEVICE 2>/dev/null) # get the corresponding mountpoint e.g. /media/USER/MINI
-            ((i++))
-            if [ -d "$MOUNT" ]; # Check if device is connected
-            then
-                printf "${MAG}mini connected, start flashing NRF52833${DEF}\n";
-                break
-            else
-                printf "${DEF}MINI device not found. Try Again $i/$TRIES ${DEF}\n";
-                sleep 1
-                if [[ "$i" == $TRIES ]]; then
-                    printf "${RED}MINI device not found. Start anew${DEF}\n";
-                    break # If mini is not found, start anew
-                fi        
-            fi
-        done
-        
-        
+	await_mount "MINI" 20 # Wait for device
+        if [ "$?" == "0" ]; then 
+	    DEVICE=$(blkid -L "MINI" 2>/dev/null)
+	    MOUNT=$(lsblk -o MOUNTPOINT -nr $DEVICE 2>/dev/null)
+	else 
+	    printf "${RED}Mounting failed ${DEF}\n";
+	    break;
+        fi
+	printf "${MAG}Start flashing NRF52833${DEF}\n";
         cp $APPLICATION_FW $MOUNT; # copy application firmware to mini
         if [[ $? = 0 ]];
         then
             printf "${GRE}NRF52833: flashed ${DEF}\n";
             printf "${GRE}SUCCESS: Programming done in $(($SECONDS - $START)) seconds. ${DEF}\n";
             echo 1 > /sys/class/leds/ACT/brightness; setLightState $APP_DONE_LED 1; # turns on APPLED and green ACT LED
+	    sudo eject $DEVICE
             
         else
             printf "${RED}NRF52833: Flashing failed ${DEF}\n"; break; break;
@@ -113,7 +118,6 @@ while true; do # First loop for always on
         COUNT=$((COUNT+1))
         echo "$DATETIME $COUNT $ELAPSED" >> prodprog.log
         
-        # Here testing needs to be implemented
         # Wait for mini disconnection
         while true; do
             JLinkExe -NoGui 1 -CommandFile on.jlink >on.log
